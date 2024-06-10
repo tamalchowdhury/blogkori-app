fb8712a04410a70e80431b4209e91ea66f2f8118
Tamal Anwar Chowdhury <tamalanwaronline@gmail.com>
2024-06-10 18:19:42 +0600

    adds latest blog post heading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,25 +10,30 @@ export default async function Home() {
   const post = await response.json()
 
   return (
-    <div className='space-y-[30px]'>
-      {post.map((p) => (
-        <div className='space-y-[20px]' key={p.id}>
-          <Link href={`/${p.slug}`}>
-            <h1
-              className={`${merriweather.className} text-[28px] leading-tight font-bold hover:underline`}
-            >
-              {p.title.rendered}
-            </h1>
-          </Link>
-          <div
-            className='text-[20px]'
-            dangerouslySetInnerHTML={{ __html: p.excerpt.rendered }}
-          />
-          <span>
-            <Link href={`/${p.slug}`}>Read More</Link>
-          </span>
-        </div>
-      ))}
-    </div>
+    <>
+      <h3 className={`${merriweather.className} uppercase -ml-[10px] mb-2 tracking-wider text-[14px]`}>
+        Latest Blog Posts
+      </h3>
+      <div className='space-y-[30px]'>
+        {post.map((p) => (
+          <div className='space-y-[20px]' key={p.id}>
+            <Link href={`/${p.slug}`}>
+              <h2
+                className={`${merriweather.className} text-[28px] leading-tight font-bold hover:underline`}
+              >
+                {p.title.rendered}
+              </h2>
+            </Link>
+            <div
+              className='text-[20px]'
+              dangerouslySetInnerHTML={{ __html: p.excerpt.rendered }}
+            />
+            <span>
+              <Link href={`/${p.slug}`}>Read More</Link>
+            </span>
+          </div>
+        ))}
+      </div>
+    </>
   )
 }
