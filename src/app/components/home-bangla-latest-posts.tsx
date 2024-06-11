773014f3abda2673eab36eb06860fdb8f1e97525
Tamal Anwar Chowdhury <tamalanwaronline@gmail.com>
2024-06-11 16:07:27 +0600

    adds custom bangla fonts

--- src/app/components/home-bangla-latest-posts.tsx
+++ src/app/components/home-bangla-latest-posts.tsx
@@ -2,6 +2,7 @@ import Link from "next/link"
 import React from "react"
 import { merriweather } from "../lib/utils"
 import { API_URL } from "../lib/consts"
+import { alinurFont, shamimFont } from "@/fonts/fonts"
 
 export default async function HomeBanglaLatestPosts() {
   const response = await fetch(`${API_URL}/posts?categories=3&per_page=3`, {
@@ -18,13 +19,13 @@ export default async function HomeBanglaLatestPosts() {
         <div className='space-y-[20px]' key={p.id}>
           <Link href={`/${p.slug}`}>
             <h2
-              className={`${merriweather.className} text-[28px] leading-tight font-bold hover:underline`}
+              className={`${alinurFont.className} text-[28px] leading-tight font-bold hover:underline`}
             >
               {p.title.rendered}
             </h2>
           </Link>
           <div
-            className='text-[16px] md:text-[20px]'
+            className={` text-[16px] md:text-[20px]`}
             dangerouslySetInnerHTML={{ __html: p.excerpt.rendered }}
           />
           <span>
