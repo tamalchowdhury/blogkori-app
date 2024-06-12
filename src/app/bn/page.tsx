7368b58fdd2934d711da662d074aaa00e1c7b3ea
Tamal Anwar Chowdhury <tamalanwaronline@gmail.com>
2024-06-12 21:29:39 +0600

    chore: add static metadata in /bn page

--- src/app/bn/page.tsx
+++ src/app/bn/page.tsx
@@ -3,10 +3,15 @@ import { BanglaTitle } from "../[slug]/single-post"
 import LoadingPosts from "../components/loading-posts"
 import HomeBanglaLatestPosts from "../components/home-bangla-latest-posts"
 
+export const metadata: Metadata = {
+  title: "বাংলা পোস্টগুলো । ব্লগকরি",
+  description: "বাংলা পোস্টগুলো । ব্লগকরি",
+}
+
 export default function BanglaPage() {
   return (
     <div>
-      <BanglaTitle className="mb-4 -ml-4" >বাংলা পোস্টগুলো</BanglaTitle>
+      <BanglaTitle className='mb-4 md:-ml-4'>বাংলা পোস্টগুলো</BanglaTitle>
       <Suspense fallback={<LoadingPosts />}>
         <HomeBanglaLatestPosts all={true} />
       </Suspense>
