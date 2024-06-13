866bb53456acd93c51ea83729e9c33ab4182e0e3
Tamal Anwar Chowdhury <tamalanwaronline@gmail.com>
2024-06-13 17:41:17 +0600

    feat: add header logo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css"
 import Link from "next/link"
 import { merriweather } from "./lib/utils"
 import Sidebar from "./components/sidebar"
+import Image from "next/image"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -26,9 +27,25 @@ export default function RootLayout({
       >
         <header className='p-8 bg-blue-300'>
           <div className='max-w-[1200px] mx-auto'>
-            <h1 className={`${merriweather.className} text-[24px]`}>
-              <Link href='/'>BlogKori</Link>
-            </h1>
+            <div className='logo flex gap-4 items-center'>
+              <div className=''>
+                <Image
+                  src='icon.svg'
+                  width={48}
+                  height={48}
+                  alt='blogkori logo'
+                  className='rounded-md object-cover '
+                />
+              </div>
+              <div>
+                <h1 className={`${merriweather.className} text-[24px]`}>
+                  <Link href='/'>BlogKori</Link>
+                </h1>
+                <div className='uppercase text-[14px] tracking-wide'>
+                  Blogging My Online Journey
+                </div>
+              </div>
+            </div>
           </div>
         </header>
         <div className='grid md:grid-cols-[650px_1fr] gap-[40px] bg-white min-h-[600px] max-w-[1050px] mx-auto md:px-[80px] md:py-[40px]'>
