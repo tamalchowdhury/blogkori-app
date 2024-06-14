9a7b14350ee5f972c4311c812c4320d14e6e5e8e
Tamal Anwar Chowdhury <tamalanwaronline@gmail.com>
2024-06-14 19:54:34 +0600

    feat: add share icons for share buttons

--- src/app/[slug]/components/more-button.tsx
+++ src/app/[slug]/components/more-button.tsx
@@ -22,7 +22,21 @@ export default function MoreButton({ url }: Props) {
       onClick={handleNativeShare}
       className='more share-button bg-slate-500/70 hover:bg-slate-500'
     >
-      More
+      More <MoreIcon />
     </div>
+  )
+}
+
+function MoreIcon({ size = 20 }) {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      viewBox='0 0 512 512'
+      width={size}
+      height={size}
+      fill='white'
+    >
+      <path d='M8 256a56 56 0 1 1 112 0A56 56 0 1 1 8 256zm160 0a56 56 0 1 1 112 0 56 56 0 1 1 -112 0zm216-56a56 56 0 1 1 0 112 56 56 0 1 1 0-112z' />
+    </svg>
   )
 }
