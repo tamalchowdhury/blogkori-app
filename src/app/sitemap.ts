cdecf2963ccf6134c7979057a9a4959afa88232b
Tamal Anwar Chowdhury <tamalanwaronline@gmail.com>
2024-06-18 21:55:33 +0600

    feat: load all the posts from the api, and add promise in type

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,12 +1,34 @@
 import { MetadataRoute } from "next"
+import { API_URL, BASE_URL } from "./lib/consts"
 
-export default function sitemap(): MetadataRoute.Sitemap {
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const response = await fetch(`${API_URL}/posts`)
+  const posts = await response.json()
+
   return [
     {
-      url: "https://www.blogkori.com",
+      url: BASE_URL,
       lastModified: new Date(),
       changeFrequency: "daily",
       priority: 1,
     },
+    {
+      url: `${BASE_URL}/en`,
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 0.9,
+    },
+    {
+      url: `${BASE_URL}/bn`,
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 0.9,
+    },
+    ...posts.map((p) => ({
+      url: `${BASE_URL}/${p.slug}`,
+      lastModified: new Date(p.date),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    })),
   ]
 }
