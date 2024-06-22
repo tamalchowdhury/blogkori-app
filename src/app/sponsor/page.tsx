698a14535adc05f757b79fb9957008e2a065e570
Tamal Anwar Chowdhury <tamalanwaronline@gmail.com>
2024-06-22 20:51:30 +0600

    chore: add css styles for post_body h2 tag

--- src/app/sponsor/page.tsx
+++ src/app/sponsor/page.tsx
@@ -6,7 +6,13 @@ export default function SponsorPage() {
       <EnglishTitle>Sponsor BlogKori</EnglishTitle>
       <div className='post__body text-[16px] md:text-[20px]'>
         <p>Support BlogKori by being a sponsor.</p>
-        <p>Thre are a few ways to sponsor this site.</p>
+        <p>There are a few ways to sponsor this site.</p>
+        <h2>Sponsored Content</h2>
+        <p>
+          Become a sponsor by purchasing a paid link in the sidebar or
+          commissioning a paid review. Please send me an email:{" "}
+          <strong>mail [at] tamalchowdhury [dot] com</strong>
+        </p>
       </div>
     </>
   )
