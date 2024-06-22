a3ac3dc90ea35462aa6bb9b47564a164da02c668
Tamal Anwar Chowdhury <tamalanwaronline@gmail.com>
2024-06-22 20:47:09 +0600

    chore: add tailwind styles for sponsor page

--- src/app/sponsor/page.tsx
+++ src/app/sponsor/page.tsx
@@ -1,3 +1,13 @@
+import { EnglishTitle } from "../components/english-title"
+
 export default function SponsorPage() {
-  return <div>Sponsor Page.</div>
+  return (
+    <>
+      <EnglishTitle>Sponsor BlogKori</EnglishTitle>
+      <div className='post__body text-[16px] md:text-[20px]'>
+        <p>Support BlogKori by being a sponsor.</p>
+        <p>Thre are a few ways to sponsor this site.</p>
+      </div>
+    </>
+  )
 }
