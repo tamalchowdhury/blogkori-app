f5616491972e6b6f6cd2d4b538c7e2159228f030
Tamal Anwar Chowdhury <tamalanwaronline@gmail.com>
2024-06-23 15:44:55 +0600

    chore: add contact page to sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -25,6 +25,12 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
       priority: 0.9,
     },
     {
+      url: `${BASE_URL}/contact`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.5,
+    },
+    {
       url: `${BASE_URL}/sponsor`,
       lastModified: new Date(),
       changeFrequency: "monthly",
