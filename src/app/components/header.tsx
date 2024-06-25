1aa94907d66552e322db159d5994caff3c176c25
Tamal Anwar Chowdhury <tamalanwaronline@gmail.com>
2024-06-25 22:23:48 +0600

    feat: add nav menu items

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -4,10 +4,16 @@ import React from "react"
 import { merriweather } from "../lib/utils"
 import { SITE_NAME, SITE_TAGLINE } from "../lib/consts"
 
+const pages = [
+  { name: "Home", url: "/" },
+  { name: "Sponsor", url: "/sponsor" },
+  { name: "Contact", url: "/contact" },
+]
+
 export default function Header() {
   return (
     <header className='p-2 md:p-4 bg-[#1c2023] text-white'>
-      <div className='max-w-[1200px] mx-auto'>
+      <div className='max-w-[1200px] space-y-4 md:space-y-0 mx-auto md:flex  items-center'>
         <div className='logo flex gap-2 md:gap-4 items-center'>
           <div className=''>
             <Link href='/'>
@@ -31,6 +37,20 @@ export default function Header() {
             </div>
           </div>
         </div>
+        <nav className='ml-auto'>
+          <ul className='flex gap-2 md:gap-8'>
+            {pages.map((page) => (
+              <li key={page.url}>
+                <Link
+                  className='text-white/70 hover:text-white transition px-2 py-1'
+                  href={page.url}
+                >
+                  {page.name}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
       </div>
     </header>
   )
