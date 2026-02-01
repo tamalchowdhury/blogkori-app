f5af342c5d8d4aeb80068bbc2e853c7e3ce21e81
Tamal Anwar Chowdhury <tamalanwaronline@gmail.com>
2026-02-01 17:42:25 +0600

    update nav menu

--- src/app/lib/menuPaths.ts
+++ src/app/lib/menuPaths.ts
@@ -1,5 +1,7 @@
 export const menuPaths = [
   { name: "Home", url: "/" },
+  { name: "English", url: "/en" },
+  { name: "বাংলা", url: "/bn" },
   { name: "Sponsor", url: "/sponsor" },
   { name: "Contact", url: "/contact" },
 ]
